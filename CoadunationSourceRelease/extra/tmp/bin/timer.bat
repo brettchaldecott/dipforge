166c0fa3d63f746f4bd21e0678e6544433887acc
brett chaldecott <bchaldecott@hugetelecom.co.za>
2010-11-27 04:39:35 +0200

    reconfigured the repository layout

--- CoadunationSourceRelease/extra/tmp/bin/timer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set JAVA_HOME='Replace with Java JDK path'
-set EXTRA="'Replace with Coadunation install directory'/lib/jacorb.jar;'Replace with Coadunation install directory'/lib/log4j-1.2.13.jar;'Replace with Coadunation install directory'/lib/logkit-1.2.jar;'Replace with Coadunation install directory'/lib/CoadunationClient.jar;'Replace with Coadunation install directory'\clientlib\TimerClient.jar;'Replace with Coadunation install directory'/tools/TimerCommandLineTool.jar;'Replace with Coadunation install directory'/clientlib/avalon-framework-4.1.5.jar;"
-set JAVA='Replace with Java runtime enviroment path'
-set JAVA_OPTS=-Djava.security.policy=="'Replace with Coadunation install directory'/etc/server.policy"
-set JAVA_OPTS=%JAVA_OPTS% -Djava.security.manager
-
-%JAVA% %JAVA_OPTS% -Xmx128M -cp %EXTRA% com.rift.coad.commandline.timercommandlinetool.Main %*
