0f0514054f6b3b0359e0e9c5ce34b25aee6a621d
Brett Chaldecott <brett.chaldecott@gmail.com>
2011-11-09 10:33:09 +0200

    Added in the publish subscriber interface.

---
+++ client/CoadunationTypeManager/src/com/rift/coad/type/subscriber/PublishSubscriber.java
@@ -0,0 +1,42 @@
+/*
+ * CoadunationTypeManage: The resource type manager.
+ * Copyright (C) 2011  Rift IT Contracting
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * PublishSubscriber.java
+ */
+package com.rift.coad.type.subscriber;
+
+import java.rmi.Remote;
+
+/**
+ * The interface for the publish subscriber object.
+ * 
+ * @author brett chaldecott
+ */
+public interface PublishSubscriber extends Remote {
+    
+    /**
+     * The service name
+     */
+    public final static String SERVICE_NAME = "publish_subscriber";
+    
+    
+    /**
+     * This method will be called when ever new RDF is published.
+     */
+    public void rdfPublished();
+}
