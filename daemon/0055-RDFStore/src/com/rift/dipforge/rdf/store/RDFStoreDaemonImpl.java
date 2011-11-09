69332bd660e3753652f338552be075d4792a42ee
Brett Chaldecott <brett.chaldecott@gmail.com>
2011-11-09 16:52:41 +0200

    Fixed the RDF store to listen to Ontology publish events and to reload the ontology when required.

--- daemon/0055-RDFStore/src/com/rift/dipforge/rdf/store/RDFStoreDaemonImpl.java
+++ daemon/0055-RDFStore/src/com/rift/dipforge/rdf/store/RDFStoreDaemonImpl.java
@@ -28,7 +28,6 @@ import com.rift.coad.lib.deployment.DeploymentMonitor;
 import com.rift.coad.lib.thread.ThreadStateMonitor;
 import com.rift.coad.rdf.semantic.Session;
 import com.rift.coad.rdf.semantic.coadunation.SemanticUtil;
-import com.rift.coad.type.subscriber.PublishSubscriber;
 import com.rift.coad.util.connection.ConnectionManager;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
@@ -40,7 +39,7 @@ import org.apache.log4j.Logger;
  *
  * @author brett chaldecott
  */
-public class RDFStoreDaemonImpl implements RDFStoreDaemon, PublishSubscriber, BeanRunnable  {
+public class RDFStoreDaemonImpl implements RDFStoreDaemon, BeanRunnable  {
 
     // class singletons.
     private Logger log = Logger.getLogger(RDFStoreDaemonImpl.class);
@@ -109,7 +108,6 @@ public class RDFStoreDaemonImpl implements RDFStoreDaemon, PublishSubscriber, Be
                     getConnection(ServiceBroker.class, "ServiceBroker");
             List<String> services = new ArrayList<String>();
             services.add(Constants.SERVICE);
-            services.add(PublishSubscriber.SERVICE_NAME);
             broker.registerService("rdf/RDFStoreDaemon", services);
         } catch (Exception ex) {
             log.error("Failed to register the entries with the service broker : " +
@@ -132,19 +130,6 @@ public class RDFStoreDaemonImpl implements RDFStoreDaemon, PublishSubscriber, Be
      */
     public void terminate() {
         monitor.terminate(true);
-    }
-
-    
-    /**
-     * This method is called when RDF is published.
-     */
-    public void rdfPublished() throws RemoteException {
-        try {
-            SemanticUtil.getInstance(RDFConfig.class).releadOntology();
-        } catch (Exception ex) {
-            log.error("Failed to reload the ontology because : " + 
-                    ex.getMessage(),ex);
-        }
     }
 
 }
