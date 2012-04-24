f8a65f8be169eec2d91e0206147f8381df6bba62
Brett Chaldecott <brett.chaldecott@gmail.com>
2012-04-24 12:50:53 +0200

    updated the typing system

--- release/dipforge/var/projects/test/controls/index.groovy
+++ release/dipforge/var/projects/test/controls/index.groovy
@@ -1,11 +1,19 @@
-import com.rift.coad.rdf.semantic.coadunation.SemanticUtil;
+import com.dipforge.utils.PageManager;
+import com.dipforge.semantic.RDF;
+import org.apache.log4j.Logger;
 
-def forward(page, req, res){
-  def dis = req.getRequestDispatcher(page);
-  dis.forward(req, res);
-}
+def log = Logger.getLogger("test.index.groovy");
+
+def test1 = RDF.create("http://dipforge.sourceforge.net/test#type1")
+
+test1.setProperty1("fred")
+test1.setProperty2("bob")
+log.info("##########################################")
+log.info("Property 1 value : " +test1.getProperty1())
+log.info("Property 2 value : " +test1.getProperty2())
+log.info("Property 2 value : " +test1)
+log.info("##########################################")
 
 
-//forward("index.gsp", request, response)
+PageManager.forward("index.gsp", request, response)
 
-println "<html><body>fred said</body></html>"
--- release/dipforge/var/projects/test/views/index.gsp
+++ release/dipforge/var/projects/test/views/index.gsp
@@ -1,8 +1,12 @@
 <%
-import test
+import com.dipforge.semantic.RDF
+import com.dipforge.request.RequestHandler
 %>
 
 <html>
+<head>
+<title>testing</title>
+</head>
 <body>
 hello world
 </body>
