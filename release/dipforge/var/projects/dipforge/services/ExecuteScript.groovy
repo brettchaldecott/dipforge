faf6d7bbdbed600f62255a47f911b39351c90b5c
Brett Chaldecott <brett.chaldecott@gmail.com>
2012-05-21 18:55:00 +0200

    continued development of the workflow rpc

---
+++ release/dipforge/var/projects/dipforge/services/ExecuteScript.groovy
@@ -0,0 +1,51 @@
+/*
+ * dipforge: The core libraries.
+ * Copyright (C) Wed Dec 07 09:16:38 SAST 2011 owner 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * ExecuteScript.groovy
+ * @author brett chaldecott
+ */
+
+// imports
+import com.dipforge.semantic.RDF;
+import com.rift.coad.change.request.RequestData;
+import com.rift.coad.rdf.semantic.types.XSDDataDictionary;
+ 
+def className = method.getClassName()
+def methodName = method.getMethodName()
+
+def classRef = "${className}".newInstance();
+
+def params = [:]
+for (int index = 0; index < parameters.size(); index++) {
+    def param = parameters.get(index)
+    def name = method.getParameters().get(index).getName()
+    if (param instanceof RequestData) {
+        param = RDF.getFromXML(param.getData(), param.getDataType() + "/" + param.getId())
+    }
+    params.put(name,param)
+}
+
+def result = classRef."${methodName}"(params)
+
+if (method.getReturnType() == null || 
+        XSDDataDictionary.isBasicTypeByURI(method.getReturnType())) {
+    return result
+} else {
+    return new RequestData(result.getId(), result.builder.classDef.getURI().toString(),
+                result.toXML(), result.builder.classDef.getLocalName())
+}
