1730805bf48581fd6bb8895df971d7a91818f815
Brett Chaldecott <brett.chaldecott@gmail.com>
2012-07-24 15:51:59 +0200

    1) Fixed the copy RDF method to handle array properties on values.

--- daemon/0170-ChangeManager/src/com/rift/coad/change/request/action/leviathan/store/LsStoreMethodStackEntry.java
+++ daemon/0170-ChangeManager/src/com/rift/coad/change/request/action/leviathan/store/LsStoreMethodStackEntry.java
@@ -23,6 +23,7 @@ package com.rift.coad.change.request.action.leviathan.store;
 
 import com.rift.coad.change.request.action.leviathan.requestdata.LsActionRDFProperty;
 import com.rift.coad.daemon.messageservice.rpc.RPCMessageClient;
+import com.rift.coad.rdf.semantic.Property;
 import com.rift.coad.rdf.semantic.RDFConstants;
 import com.rift.coad.rdf.semantic.Resource;
 import com.rift.coad.rdf.semantic.Session;
@@ -39,9 +40,11 @@ import com.rift.dipforge.rdf.store.master.MasterRDFStoreDaemon;
 import com.rift.dipforge.rdf.store.master.MasterRDFStoreDaemonAsync;
 import com.rift.dipforge.rdf.store.master.StoreActions;
 import java.io.Serializable;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.apache.log4j.Logger;
 
 /**
  * The implementation of the LsStoreMethod stack entry
@@ -49,7 +52,9 @@ import java.util.Map;
  * @author brett chaldecott
  */
 public class LsStoreMethodStackEntry extends ProcessStackEntry {
-
+    
+    private static Logger log = Logger.getLogger(LsStoreMethodStackEntry.class);
+    
     // private memember variables
     private CallStatement callStatement;
     private LsStoreProperty target;
@@ -191,96 +196,97 @@ public class LsStoreMethodStackEntry extends ProcessStackEntry {
             Resource resultResource = resultSession.createResource(
                     ontologyClass.getURI(), sourceResource.getURI());
             for (OntologyProperty property : ontologyClass.listProperties()) {
-
+                
+                
                 // if this does not have a range just add the property.
                 String propertyURI = property.getURI().toString();
-                
-                if (!sourceResource.hasProperty(propertyURI)) {
-                    continue;
-                }
-                
-                if (!property.hasRange()) {
-                    Resource sourceSubResource = sourceResource.getProperty(Resource.class,
-                            propertyURI);
-                    if (resultSession.contains(Resource.class,
-                            sourceSubResource.getURI().toString())) {
-                        resultResource.addProperty(propertyURI,
-                                resultSession.get(Resource.class,
-                                sourceSubResource.getURI().toString()));
-                    } else {
-                        resultResource.addProperty(propertyURI,
-                                resultSession.createResource(
-                                sourceSubResource.getURI()));
+                List propertyValues = sourceResource.listProperties(propertyURI);
+                for (Object objPropertyValue : propertyValues) {
+                    Property propertyValue = (Property)objPropertyValue;
+                    if (!sourceResource.hasProperty(propertyURI)) {
+                        continue;
                     }
-                    continue;
-                }
-                
-                String propertyType = property.getType().getURI().toString();
-                if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_STRING).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(String.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_BOOLEAN).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Boolean.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_FLOAT).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Float.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_DOUBLE).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Double.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_INTEGER).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Integer.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_LONG).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Long.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_INT).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Boolean.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_SHORT).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Short.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_BYTE).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Short.class,
-                                propertyURI));
-                } else if (propertyType.equals(
-                        XSDDataDictionary.getTypeByName(
-                        XSDDataDictionary.XSD_DATE).getURI().toString())) {
-                    resultResource.addProperty(propertyURI,
-                                sourceResource.getProperty(Short.class,
-                                propertyURI));
-                } else {
-                    resultResource.addProperty(propertyURI,
-                                copyResource(sourceSession,resultSession,
-                                sourceResource.getProperty(Resource.class,
-                                propertyURI)));
+
+                    if (!property.hasRange()) {
+                        Resource sourceSubResource = propertyValue.get(Resource.class);
+                        if (resultSession.contains(Resource.class,
+                                sourceSubResource.getURI())) {
+                            resultResource.addProperty(propertyURI,
+                                    resultSession.get(Resource.class,
+                                    sourceSubResource.getURI()));
+                        } else {
+                            String targetUri = sourceSubResource.get(String.class);
+                            log.info("##########################");
+                            log.info("The property URI is : " + propertyURI);
+                            log.info("The property URI is : " + property.getType().getURI());
+                            log.info("The target uri is : " + targetUri);
+                            log.info("##########################");
+                            resultResource.addProperty(propertyURI,
+                                    resultSession.createResource(
+                                    property.getType().getURI(),
+                                    new URI(targetUri)));
+                        }
+                        continue;
+                    }
+
+                    String propertyType = property.getType().getURI().toString();
+                    if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_STRING).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(String.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_BOOLEAN).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Boolean.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_FLOAT).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Float.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_DOUBLE).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Double.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_INTEGER).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Integer.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_LONG).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Long.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_INT).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Boolean.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_SHORT).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Short.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_BYTE).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Short.class));
+                    } else if (propertyType.equals(
+                            XSDDataDictionary.getTypeByName(
+                            XSDDataDictionary.XSD_DATE).getURI().toString())) {
+                        resultResource.addProperty(propertyURI,
+                                    propertyValue.get(Short.class));
+                    } else {
+                        log.info("#### add new resource " + 
+                                propertyValue.get(Resource.class).getURI().toString());
+                        resultResource.addProperty(propertyURI,
+                                    copyResource(sourceSession,resultSession,
+                                    propertyValue.get(Resource.class)));
+                    }
                 }
             }
             return resultResource;
