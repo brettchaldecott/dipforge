6a9dd7c7c2910bf6a29f1a693e95f78162708a62
Brett Chaldecott <brett.chaldecott@gmail.com>
2012-12-01 06:41:16 +0200

    added query for duplicate name check

--- release/dipforge/var/projects/bss/controls/user/CreateUser.groovy
+++ release/dipforge/var/projects/bss/controls/user/CreateUser.groovy
@@ -35,115 +35,125 @@ def log = Logger.getLogger("com.dipforge.log.user.CreateUser");
 log.info("Create a catalog entry")
 log.info(params)
 
-// perform a check for a duplicate
-def result = RDF.query("SELECT ?s WHERE {" +
-    "?s a <http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Offering#Offering> . " +
-    "?s <http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Offering#id> ?id . "+
-    "FILTER (?id = \"${params.userType}\")}")
-
-if (result.size() >= 1) {
+def users = RDF.query("SELECT ?d WHERE {" +
+    "?s a <http://dipforge.sourceforge.net/schema/rdf/1.0/oss/User#User> . " +
+    "?d a <http://dipforge.sourceforge.net/schema/rdf/1.0/oss/Desktop#Desktop> . " +
+    "?s <http://dipforge.sourceforge.net/schema/rdf/1.0/oss/User#username> ?username . " + 
+    "?d <http://dipforge.sourceforge.net/schema/rdf/1.0/oss/Desktop#user> ?s . " + 
+    "FILTER (?username = '${params.username}')} " +
+    "ORDER BY ?username ")
+if (users.size() == 0) {
+    // perform a check for a duplicate
+    def result = RDF.query("SELECT ?s WHERE {" +
+        "?s a <http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Offering#Offering> . " +
+        "?s <http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Offering#id> ?id . "+
+        "FILTER (?id = \"${params.userType}\")}")
     
-    def offering = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Offering#Offering/${params.userType}")
-    def offeringUtil = new OfferingUtil(offering)
-    def organisation = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Organisation#Organisation/base")
-    
-    def organisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOffering#OrganisationOffering")
-    organisationOffering.setId("base-${params.userId}")
-    organisationOffering.setOffering(offering)
-    organisationOffering.setOrganisation(organisation)
-    organisationOffering.setInstalled(new java.util.Date())
-    organisationOffering.setStatus("active")
-    
-    def request = RequestHandler.getInstance("bss", "CreateOrganisationOffering", organisationOffering)
-    
-    // get hold of the user from the offering
-    def userConfig = offeringUtil.getProductConfig("user")
-    def principals = userConfig.getData()
-    
-    def user = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/User#User");
-    user.setId(params.userId)
-    user.setUsername(params.username)
-    user.setEmail(params.email)
-    user.setPassword(params.password)
-    user.setUserType(params.userType)
-    user.setPrincipals(principals)
-    user.setCreated(new java.util.Date())
-    user.setModified(new java.util.Date())
-    
-    def userRequest = request.createChild('oss', 'CreateUser', user)
-    
-    def userProduct = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Product#Product/user")
-    def userOrganisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOfferingComponent#OrganisationOfferingComponent")
-    userOrganisationOffering.setId("user-${params.userId}")
-    userOrganisationOffering.setProduct(userProduct)
-    userOrganisationOffering.setUri("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/User#User/${params.userId}")
-    userOrganisationOffering.setOrganisationOffering(organisationOffering)
-    
-    userRequest.createChild('bss', 'CreateOrganisationOfferingComponent', userOrganisationOffering)
-    
-    def desktop = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/Desktop#Desktop");
-    
-    desktop.setId(params.userId)
-    def desktopConfig = offeringUtil.getProductConfig("desktop")
-    desktop.setName(desktopConfig.getData())
-    desktop.setCreated(new java.util.Date())
-    desktop.setModified(new java.util.Date())
-    desktop.setUser(user)
-    
-    def desktopRequest = userRequest.createChild('oss', 'CreateDesktop', desktop)
-    
-    def desktopOrganisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOfferingComponent#OrganisationOfferingComponent")
-    def desktopProduct = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Product#Product/desktop")
-    desktopOrganisationOffering.setId("desktop-${params.userId}")
-    desktopOrganisationOffering.setProduct(desktopProduct)
-    desktopOrganisationOffering.setUri("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/Desktop#Desktop/${params.userId}")
-    desktopOrganisationOffering.setOrganisationOffering(organisationOffering)
-    
-    desktopRequest.createChild('bss', 'CreateOrganisationOfferingComponent', desktopOrganisationOffering)
-    
-    // create the applications
-    def applicationConfig = offeringUtil.getProductConfig("application")
-    def appList = applicationConfig.getData().split(":")
-    def applicationProduct = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Product#Product/application")
-    appList.each { appEntry ->
-        def desktopApplication = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/DesktopApplication#DesktopApplication");
+    if (result.size() >= 1) {
         
-        def appInfo = processAppEntry(log, appEntry)
-        def appName = appInfo["name"]
-        def appStripName = stripSpace(appName)
-        desktopApplication.setId("application-${params.userId}-${appStripName}")
-        desktopApplication.setName(appName)
-        desktopApplication.setUrl(appInfo["url"])
-        desktopApplication.setPrincipal(appInfo["principle"])
-        desktopApplication.setThumbnail(appInfo["thumbnail"])
-        desktopApplication.setIcon(appInfo["icon"])
-        desktopApplication.setCreated(new java.util.Date())
-        desktopApplication.setModified(new java.util.Date())
-        desktopApplication.setDesktop(desktop)
-        desktopApplication.setUser(user)
+        def offering = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Offering#Offering/${params.userType}")
+        def offeringUtil = new OfferingUtil(offering)
+        def organisation = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Organisation#Organisation/base")
         
-        def applicationRequest = 
-            desktopRequest.createChild('oss', 'CreateDesktopApplication', desktopApplication)
+        def organisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOffering#OrganisationOffering")
+        organisationOffering.setId("base-${params.userId}")
+        organisationOffering.setOffering(offering)
+        organisationOffering.setOrganisation(organisation)
+        organisationOffering.setInstalled(new java.util.Date())
+        organisationOffering.setStatus("active")
+        
+        def request = RequestHandler.getInstance("bss", "CreateOrganisationOffering", organisationOffering)
+        
+        // get hold of the user from the offering
+        def userConfig = offeringUtil.getProductConfig("user")
+        def principals = userConfig.getData()
+        
+        def user = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/User#User");
+        user.setId(params.userId)
+        user.setUsername(params.username)
+        user.setEmail(params.email)
+        user.setPassword(params.password)
+        user.setUserType(params.userType)
+        user.setPrincipals(principals)
+        user.setCreated(new java.util.Date())
+        user.setModified(new java.util.Date())
+        
+        def userRequest = request.createChild('oss', 'CreateUser', user)
+        
+        def userProduct = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Product#Product/user")
+        def userOrganisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOfferingComponent#OrganisationOfferingComponent")
+        userOrganisationOffering.setId("user-${params.userId}")
+        userOrganisationOffering.setProduct(userProduct)
+        userOrganisationOffering.setUri("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/User#User/${params.userId}")
+        userOrganisationOffering.setOrganisationOffering(organisationOffering)
+        
+        userRequest.createChild('bss', 'CreateOrganisationOfferingComponent', userOrganisationOffering)
+        
+        def desktop = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/Desktop#Desktop");
+        
+        desktop.setId(params.userId)
+        def desktopConfig = offeringUtil.getProductConfig("desktop")
+        desktop.setName(desktopConfig.getData())
+        desktop.setCreated(new java.util.Date())
+        desktop.setModified(new java.util.Date())
+        desktop.setUser(user)
+        
+        def desktopRequest = userRequest.createChild('oss', 'CreateDesktop', desktop)
+        
+        def desktopOrganisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOfferingComponent#OrganisationOfferingComponent")
+        def desktopProduct = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Product#Product/desktop")
+        desktopOrganisationOffering.setId("desktop-${params.userId}")
+        desktopOrganisationOffering.setProduct(desktopProduct)
+        desktopOrganisationOffering.setUri("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/Desktop#Desktop/${params.userId}")
+        desktopOrganisationOffering.setOrganisationOffering(organisationOffering)
+        
+        desktopRequest.createChild('bss', 'CreateOrganisationOfferingComponent', desktopOrganisationOffering)
+        
+        // create the applications
+        def applicationConfig = offeringUtil.getProductConfig("application")
+        def appList = applicationConfig.getData().split(":")
+        def applicationProduct = RDF.getFromStore("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/Product#Product/application")
+        appList.each { appEntry ->
+            def desktopApplication = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/DesktopApplication#DesktopApplication");
             
+            def appInfo = processAppEntry(log, appEntry)
+            def appName = appInfo["name"]
+            def appStripName = stripSpace(appName)
+            desktopApplication.setId("application-${params.userId}-${appStripName}")
+            desktopApplication.setName(appName)
+            desktopApplication.setUrl(appInfo["url"])
+            desktopApplication.setPrincipal(appInfo["principle"])
+            desktopApplication.setThumbnail(appInfo["thumbnail"])
+            desktopApplication.setIcon(appInfo["icon"])
+            desktopApplication.setCreated(new java.util.Date())
+            desktopApplication.setModified(new java.util.Date())
+            desktopApplication.setDesktop(desktop)
+            desktopApplication.setUser(user)
+            
+            def applicationRequest = 
+                desktopRequest.createChild('oss', 'CreateDesktopApplication', desktopApplication)
+                
+            
+            def applicationOrganisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOfferingComponent#OrganisationOfferingComponent")
+            applicationOrganisationOffering.setId("application-${params.userId}-${appStripName}")
+            applicationOrganisationOffering.setProduct(applicationProduct)
+            applicationOrganisationOffering.setUri("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/DesktopApplication#DesktopApplication/application-${params.userId}-${appName}")
+            applicationOrganisationOffering.setOrganisationOffering(organisationOffering)
+            
+            applicationRequest.createChild('bss', 'CreateOrganisationOfferingComponent', applicationOrganisationOffering)
+            
+        }
         
-        def applicationOrganisationOffering = RDF.create("http://dipforge.sourceforge.net/schema/rdf/1.0/bss/OrganisationOfferingComponent#OrganisationOfferingComponent")
-        applicationOrganisationOffering.setId("application-${params.userId}-${appStripName}")
-        applicationOrganisationOffering.setProduct(applicationProduct)
-        applicationOrganisationOffering.setUri("http://dipforge.sourceforge.net/schema/rdf/1.0/oss/DesktopApplication#DesktopApplication/application-${params.userId}-${appName}")
-        applicationOrganisationOffering.setOrganisationOffering(organisationOffering)
+        request.makeRequest()
         
-        applicationRequest.createChild('bss', 'CreateOrganisationOfferingComponent', applicationOrganisationOffering)
+        print "success"
+    } else {
         
+        print "Failed to create the user, no matching product ${params.userType}"
     }
-    
-    request.makeRequest()
-    
-    print "success"
 } else {
-    
-    print "Failed to create the user, no matching product ${params.userType}"
+    print "Failed to create the user, duplicate user ${params.username}"
 }
-
 
 def processAppEntry(def log, def appEntry) {
     log.info("Entry : " + appEntry)
