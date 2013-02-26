5f1fffce52b0ca2e034e538a774e607227c325f6
Brett Chaldecott <brett.chaldecott@gmail.com>
2013-02-26 04:18:08 +0200

    corrected comment

--- release/dipforge/var/projects/oss/services/com/dipforge/mail/CreateMail.groovy
+++ release/dipforge/var/projects/oss/services/com/dipforge/mail/CreateMail.groovy
@@ -31,7 +31,7 @@ import org.apache.log4j.Logger;
 import com.rift.coad.daemon.email.EMailServerMBean;
 
 /**
- * This object is responsible for creating a new user
+ * This object is responsible for creating a new mail domain
  */
 class CreateMail {
     
