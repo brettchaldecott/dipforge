51fdb155502f955b1c59be503485d9300dd2ffa2
Brett Chaldecott <brett.chaldecott@gmail.com>
2013-06-17 07:25:31 +0200

    1) Changed transaction director back to bind

--- libs/CoadunationLib/src/com/rift/coad/lib/transaction/TransactionDirector.java
+++ libs/CoadunationLib/src/com/rift/coad/lib/transaction/TransactionDirector.java
@@ -69,7 +69,7 @@ public class TransactionDirector {
      */
     public TransactionDirector() throws TransactionException {
         try {
-            jotm = new Jotm(true,false);
+            jotm = new Jotm(true,true);
             ContextManager contextManager = 
                     new ContextManager(TRANSACTION_CONTEXT_BASE);
             userTransaction = jotm.getUserTransaction();
