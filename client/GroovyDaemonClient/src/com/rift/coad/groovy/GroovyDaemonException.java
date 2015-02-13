b745b5c32c1dc31fc35a0d3f8bfef05b41bb1a53
Brett Chaldecott <brett.chaldecott@gmail.com>
2015-02-13 04:54:56 +0200

    Changed the groovy daemon exception to include a string version of the stack trace

--- client/GroovyDaemonClient/src/com/rift/coad/groovy/GroovyDaemonException.java
+++ client/GroovyDaemonClient/src/com/rift/coad/groovy/GroovyDaemonException.java
@@ -28,6 +28,8 @@ package com.rift.coad.groovy;
  */
 public class GroovyDaemonException extends Exception implements java.io.Serializable {
 
+    private String stringStack;
+    
     /**
      * Creates a new instance of <code>GroovyDaemonException</code> without detail message.
      */
@@ -52,4 +54,38 @@ public class GroovyDaemonException extends Exception implements java.io.Serializ
     public GroovyDaemonException(String msg, Throwable ex) {
         super(msg,ex);
     }
+
+    
+    /**
+     * Constructs an instance of <code>GroovyDaemonException</code> with the specified detail message.
+     * @param msg the detail message.
+     * @param ex The cause of this exception.
+     */
+    public GroovyDaemonException(String msg, String stringStack) {
+        super(msg);
+        this.stringStack = stringStack;
+    }
+    
+    
+    /**
+     * The getter for the string stack method.
+     * 
+     * @return The string stack
+     */
+    public String getStringStack() {
+        return stringStack;
+    }
+
+    
+    /**
+     * The setter for the string stack.
+     * 
+     * @param stringStack The reference to the new string stack
+     */
+    public void setStringStack(String stringStack) {
+        this.stringStack = stringStack;
+    }
+    
+    
+    
 }
