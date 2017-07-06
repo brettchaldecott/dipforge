0a46ee4742288ca2520fa642de5053d8994d5d44
Brett Chaldecott <brett.chaldecott@gmail.com>
2017-07-06 13:06:58 +0000

    1) Updated the react template

--- release/dipforge/var/template/project_react/node_project/src/index.js
+++ release/dipforge/var/template/project_react/node_project/src/index.js
@@ -2,9 +2,12 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var browserHistory = ReactRouter.browserHistory;
 
+
+console.log("We will now render")
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/" component={Hello}/>
+    <Route path="**/index.html" component={Hello}/>
   </Router>,
   document.getElementById('root')
 );
+console.log("After render")
