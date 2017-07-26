c7c09c0560e2da173ab5d0040225617439d3e7a6
Brett Chaldecott <brett.chaldecott@gmail.com>
2017-07-26 20:34:17 +0000

    1) Updated the links to include the ide2 and swagger. 2) added the create links to the izpack. 3) excluded links from the maven task

--- release/template/bin/create_links.sh
+++ release/template/bin/create_links.sh
@@ -10,3 +10,7 @@ export VERSION
 ln -s ${DIPFORGE_HOME}/var/template/project_angular/node_project/app/ ${DIPFORGE_HOME}/var/template/project_angular/views/app
 # create react project link
 ln -s ${DIPFORGE_HOME}/var/template/project_react/node_project/src/ ${DIPFORGE_HOME}/var/template/project_react/views/app
+# create links for ide2
+ln -s ${DIPFORGE_HOME}/var/projects/ide2/node_project/app ${DIPFORGE_HOME}/var/projects/ide2/views/app
+# create links for swagger
+ln -s ${DIPFORGE_HOME}/var/projects/swagger/node-project/app ${DIPFORGE_HOME}/var/projects/swagger/views/app
