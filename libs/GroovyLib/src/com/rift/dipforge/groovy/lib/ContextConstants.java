0da7698200304ac87994f9e509a3b620a2e2fcad
Brett Chaldecott <brett.chaldecott@gmail.com>
2017-08-25 12:47:25 +0000

    1) Added extra constant class

---
+++ libs/GroovyLib/src/com/rift/dipforge/groovy/lib/ContextConstants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rift.dipforge.groovy.lib;
+
+/**
+ *
+ * @author brett chaldecott
+ */
+public class ContextConstants {
+    
+    public final static String PUBLIC_PROJECT = "public/project_";
+    
+    // format 
+    public final static String PUBLIC_PROJECT_FORMAT = "public/project_%s";
+}
